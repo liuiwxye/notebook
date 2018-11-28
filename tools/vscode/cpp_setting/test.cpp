9fcdbd51ba152e6b85a4ac4a086a4a71d1d5a554
liuwxye <857205436@qq.com>
2018-11-28 15:43:23 +0800

    Add files via upload

---
+++ tools/vscode/cpp_setting/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout<<"hello"<<std::endl;
+    int a;
+    char c;
+    cin>>a;
+    cout<<a<<endl;
+    cin>>c;
+    return 0;
+}
---
+++ tools/vscode/python_setting/test.py
@@ -0,0 +1,15 @@
+"""
+@author: liuwxye
+created at 2018-11-28
+"""
+
+import numpy as np
+
+if __name__ == "__main__":
+    # x = [1, 2, 3]
+    # y = [2, 3, 4]
+    # plt.scatter(x,y)
+    # plt.show()
+    for i in range(10):
+        print("hello")
+        print("world")
